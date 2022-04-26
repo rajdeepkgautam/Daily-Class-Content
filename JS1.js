1d22df2143fd33c3c16807f4132ddd733952f152
RAJDEEP KUMAR GAUTAM <71515360+rajdeepkgautam@users.noreply.github.com>
2022-04-26 12:43:13 +0530

    Add files via upload

---
+++ JS1.js
@@ -0,0 +1,18 @@
+console.log("Working...");
+console.log("Hello World!!!);
+var number = 8;
+console.log(number);
+function ab(n)
+{
+    console.log(n);
+    var a = n * n;
+    return a;
+}
+var newNumber = ab(5); 
+
+let a = 20;
+let b = 40;
+let add = a+b;          
+let subtract = a-b;     
+console.log(add);
+console.log(subtract);
---
+++ JS2.js
@@ -0,0 +1,30 @@
+console.log("Working...")
+
+// Boolean - (True /False), Strings - (Words, sentence, mix of numbers and words(ex - address)), Number - (integer and decimal values), 2 ,4, 101, 23.50, 34.99 
+
+"2, somelane, somecity - 711101"
+'2, somelane, somecity - 711101'
+"SIdhant", "907654321"
+
+// data types present in JS(Primitive)
+// WHat are Boolean Values - It has only two values True/False
+
+// What are Strings - Anything which is present inside the double quotes or the single  quotes are called as strings
+
+// Numbers - includes Integer and decimal values (2/3/34.66/99.99)
+
+// Variables - They are used to store the values which could be of any type i.e. Number type or String Type or Boolean type.
+
+// rules for assigning names to variables
+// try to make the variables name declarative
+// it uses Camel Case
+// Variable names are case sensitive 
+
+// fName, fname, Fname, fnAme, fnaMe, 
+
+// Keywords are of three types
+// let, var, const
+
+// -----declaraing / initializing/ redeclaring----
+
+// let - These keywords are used to as
